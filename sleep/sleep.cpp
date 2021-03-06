3c9ee7fd7f810fa0ad1e494646ff92fdaac135be
maidis <ozbekanil@gmail.com>
2021-03-06 23:24:23 +0300

    slepp: add more checks for time intervals

--- sleep/sleep.cpp
+++ sleep/sleep.cpp
@@ -24,36 +24,42 @@ int main(int argc, char *argv[])
     }
     else
     {
+        int number;
         for(int i = 1; i < argc; ++i)
         {
             std::string str = argv[i];
-            int number;
-            if(str.ends_with('s'))
+            
+            if(auto pos = str.find_first_of('s'); str.ends_with('s') && pos == str.size() - 1)
             {
                 std::from_chars(str.data(), str.data()+str.size()-1, number);
             }
-            else if(str.ends_with('m'))
+            else if(auto pos = str.find_first_of('m'); str.ends_with('m') && pos == str.size() - 1)
             {
                 std::from_chars(str.data(), str.data()+str.size()-1, number);
                 number = number * 60;
             }
-            else if(str.ends_with('h'))
+            else if(auto pos = str.find_first_of('h'); str.ends_with('h') && pos == str.size() - 1)
             {
                 std::from_chars(str.data(), str.data()+str.size()-1, number);
                 number = number * 60 * 60;
             }
-            else if(str.ends_with('d'))
+            else if(auto pos = str.find_first_of('d'); str.ends_with('d') && pos == str.size() - 1)
             {
                 std::from_chars(str.data(), str.data()+str.size()-1, number);
                 number = number * 60 * 60 * 24;
             }
-            else
+            else if(str.find_first_not_of("0123456789") == std::string::npos)
             {
                 std::from_chars(str.data(), str.data()+str.size(), number);
             }
-            
-            std::this_thread::sleep_for(std::chrono::seconds(number));
+            else
+            {
+                std::cout << "sleep: invalid time interval " << str << '\n';
+                std::cout << "Try 'sleep --help' for more information.\n";
+                return 0;
+            }
         }
+        std::this_thread::sleep_for(std::chrono::seconds(number));
     }
     return 0;
 }
