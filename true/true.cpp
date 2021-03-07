d5448c51707c4dfe11b6807705d64d6017f2159d
maidis <ozbekanil@gmail.com>
2021-03-07 21:04:23 +0300

    add true

---
+++ true/true.cpp
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/8867871/should-i-return-exit-success-or-0-from-main
+
+// true - do nothing, successfully
+// Exit with a status code indicating success.
+
+int main()
+{
+    return 0;
+}
